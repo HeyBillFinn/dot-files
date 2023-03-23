4498eb55cf92d676e7c3636bd6311c29935f4989
Bill Finn <billfinn@stripe.com>
2023-03-22 19:51:13 -0500

    add_quote: Adapt for mac

--- bin/add_quote.sh
+++ bin/add_quote.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
 # copyq clipboard | sed '${/^$/d;}' | sed 's/^/> /' | xargs -0 copyq copy > /dev/null
-xclip -selection clipboard -o | sed '${/^$/d;}' | sed 's/^/> /' | xargs -0 copyq copy > /dev/null
+# xclip -selection clipboard -o | sed '${/^$/d;}' | sed 's/^/> /' | xargs -0 copyq copy > /dev/null
+pbpaste | sed '${/^$/d;}' | sed 's/^/> /' | pbcopy > /dev/null
